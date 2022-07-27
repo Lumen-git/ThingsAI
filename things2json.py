b3405d4baced756fe25960ccdeff6df225a65b49
PinkQuartz <aimoran@protonmail.com>
2022-07-26 20:24:28 -0500

    Finishing things2json.py

--- things2json.py
+++ things2json.py
@@ -1,14 +1,21 @@
 import os
 import json
-import cv2
 
 
 def main():
+    #By defult, /Thigns is where all the images are stored
     path = "./Things"
     files = os.listdir(path)
-#    jsonData = json.dumps(files, indent=4)
-
-
+    #Create dictionary giving each item an ID
+    all_things = {}
+    i = 0
+    for item in files:
+        all_things[i] = "/Things/" + item
+        i += 1
+    #save all_things to a json file
+    with open('things.json', 'w') as fp:
+        json.dump(all_things, fp, indent=4)
+    
 
 if __name__ == "__main__":
    main()
