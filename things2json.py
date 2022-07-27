5dbf1af1f8b41b1a9504e6f68e59cb2c99621caf
Aidan Moran <aimoran@protonmail.com>
2022-07-26 19:39:25 -0500

    Added VERY EARLY AND NOT WORKING things2json.py
    literally just some test code

---
+++ things2json.py
@@ -0,0 +1,14 @@
+import os
+import json
+import cv2
+
+
+def main():
+    path = "./Things"
+    files = os.listdir(path)
+#    jsonData = json.dumps(files, indent=4)
+
+
+
+if __name__ == "__main__":
+   main()
